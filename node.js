57015a59e31c67f2517b2396c626833bbd5236b0
denyskolodych <denys.kolodych@student.karazin.ua>
2025-07-05 13:33:51 +0200

    Update node.js

--- node.js
+++ node.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 app.use(express.static('public'))
 app.use(cors())
 app.use(express.json());
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const fs = require('fs')
 let string = ""
 app.get('/words/:num', (req,res,next) =>{
